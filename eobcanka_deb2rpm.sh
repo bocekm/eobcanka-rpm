586227b43f1d7044bdee82595fefe8759e413fe8
Michal Bocek <mbocek@redhat.com>
2024-10-14 20:58:43 +0200

    Remove dependency on openssl1.1
    The eObcanka deb package 3.4.2 switched to using openssl3.0.

--- eobcanka_deb2rpm.sh
+++ eobcanka_deb2rpm.sh
@@ -31,20 +31,6 @@ pushd "$SPEC_DIR" > /dev/null
 
 SPEC=$(ls ./*.spec)
 
-fedora_version=$(grep -oP '(?<=^VERSION_ID=).+' /etc/os-release)
-if [ "fedora_version" -le 39 ]; then
-  # Replace the libcrypto library that comes with the deb package with a symlink to the system one to avoid error:
-  #     symbol lookup error: /lib64/libk5crypto.so.3: undefined symbol: EVP_KDF_ctrl, version OPENSSL_1_1_1b
-  # See: https://forum.mojefedora.cz/t/eobcanka/7941/8 or https://bugzilla.redhat.com/show_bug.cgi?id=1829790#c9
-  # Fedora 40 does not ship openssl1.1 anymore but newer versions of the eObcanka package (such as 3.4.2) work with
-  #  openssl3.0 anyway.
-  rm "opt/eObcanka/lib/openssl1.1/libcrypto.so.1.1"
-  sed -i '/^%postun/ i ln -fs /usr/lib64/libcrypto.so.1.1 /opt/eObcanka/lib/openssl1.1/libcrypto.so.1.1' "$SPEC"
-  sed -i 's/^\(".*\/libcrypto.so.1.1"\)$/%ghost \1/' "$SPEC"
-  # Make sure the openssl1.1 package is installed on the system
-  sed -i '/^Group:/ a Requires: openssl1.1' "$SPEC"
-fi
-
 # Require Czech locale package and remove Debian-specific local-gen.
 # The piece of code we want to remove from the spec file is:
 # ```
